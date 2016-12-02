6f589bdbb93ca58144c8fd3c200f6d2a53912e95
Fabfm4 <FabVinco@VincoOrbiss-MacBook-Air.local>
2016-12-02 10:40:01 -0600

    init

---
+++ PruebaKYDrawer/PruebaKYDrawer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PruebaKYDrawer
+//
+//  Created by FabVinco on 12/2/16.
+//  Copyright © 2016 FabVinco. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
